50315c77cb81f8665aafb53d16f8d758a450272a
Kaleb Bradford <131017785+LampofDiogenes@users.noreply.github.com>
2024-03-28 15:17:46 -0600

    attempt req1

--- test_cases/req1.py
+++ test_cases/req1.py
@@ -1,1 +1,11 @@
 # this will test requirement 1: Program must have a variable which controls what permissions each user has
+# just test to make sure that each person in the database has a permission setting
+
+import messenger.interact as i
+import messenger.control as c
+
+
+access = c.Control()
+
+for user in i.userlist:
+    if (user[2] is not in )
