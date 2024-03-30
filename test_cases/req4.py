485f5c402dd13f55fcec27f17da88418ec1e8a29
Kaleb Bradford <131017785+LampofDiogenes@users.noreply.github.com>
2024-03-30 13:54:57 -0600

    req 4 done

--- test_cases/req4.py
+++ test_cases/req4.py
@@ -4,13 +4,15 @@ from messenger import control
 import unittest
 
 
-class test_req4():
+class test_req4(unittest.TestCase):
 
-    control.Control.read_access()
-    control.Control.write_access()
+    # control.Control.read_access()
+    # control.Control.write_access()
 
 
-    asset_levels = control.Control.get_control()
+    asset = control.Control()
+    asset_levels = asset.get_control()
+
 
     users = [
         ['bob', asset_levels[0]], 
@@ -27,4 +29,31 @@ class test_req4():
     ]
     
     def test_req(self):
-        self.assertTrue(control.Control.read_access())
+
+        # tests to see if the permissions are the same for the user and the asset
+        # mostly just to test if this code approach works so I can use it later
+        self.assertTrue(test_req4.users[1][1] == test_req4.assets[1][1])
+        
+    def test_req2(self):
+
+        # this just tests to see if the permissions for the specified user
+        # are at a lower index than than the permssion for the specified asset
+        self.assertTrue(test_req4.asset_levels.index(test_req4.users[2][1]) 
+                        <
+                        test_req4.asset_levels.index(test_req4.assets[3][1]))
+        
+    def test_req3(self):
+
+        # same test, just moved down one
+        self.assertTrue(test_req4.asset_levels.index(test_req4.users[1][1]) 
+                        <
+                        test_req4.asset_levels.index(test_req4.assets[2][1]))
+
+
+
+test = test_req4()
+test.test_req()
+
+
+if __name__ == '__main__':
+    unittest.main()
